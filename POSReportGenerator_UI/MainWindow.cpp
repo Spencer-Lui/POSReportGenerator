3be82ef1c70989983c25aa28543f6dc8a0a033ba
Spencer-Lui <guitar227261045@gmail.com>
2026-07-19 00:17:38 +0800

    Move UI rendering into MainWindow

--- POSReportGenerator_UI/MainWindow.cpp
+++ POSReportGenerator_UI/MainWindow.cpp
@@ -1,1 +1,20 @@
 #include "MainWindow.h"
+
+#include "imgui.h"
+
+void MainWindow::Draw()
+{
+    ImGui::Begin("POS Report Generator");
+
+    ImGui::Text("Welcome to POS Report Generator!");
+
+    ImGui::Separator();
+
+    ImGui::Button("Import CSV");
+
+    ImGui::SameLine();
+
+    ImGui::Button("Export Excel");
+
+    ImGui::End();
+}
--- POSReportGenerator_UI/MainWindow.h
+++ POSReportGenerator_UI/MainWindow.h
@@ -1,5 +1,9 @@
 #pragma once
+
 class MainWindow
 {
+public:
+
+    void Draw();
 };
 
